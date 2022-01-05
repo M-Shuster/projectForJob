a7fcf2b2335352f827aafa59e42a6db223294196
Michael Shuster <michaelshuster@michaels-mbp.home>
2022-01-05 07:12:55 +0000

    minor change to homepage

--- my-app/src/Pages/Home.jsx
+++ my-app/src/Pages/Home.jsx
@@ -1,27 +1,25 @@
-import { Header } from '../Components/Header';
-import { Footer } from '../Components/Footer';
-import '../App.css';
-import { Link } from "react-router-dom"
-import {DescriptionText} from '../Components/Description'
-import { ButtonGroup, NoButton, YesButton } from '../Components/Button.styled';
+import { Header } from "../Components/Header";
+import { Footer } from "../Components/Footer";
+import "../App.css";
+import { Link } from "react-router-dom";
+import { DescriptionText } from "../Components/Description";
+import { ButtonGroup, NoButton, YesButton } from "../Components/Button.styled";
 
 export function Home() {
-
   return (
     <div className="Home">
       <Header />
-      <DescriptionText/>
-    <ButtonGroup>
-      <Link to="/cryptocurrencies">
-        <YesButton> Yes </YesButton>
-      </Link>
-      <Link to="/dead-end">
-        <NoButton> No </NoButton>
-      </Link>
-    </ButtonGroup>
-      <Footer />
+      <DescriptionText />
+      <ButtonGroup>
+        <Link to="/cryptocurrencies">
+          <YesButton> Yes </YesButton>
+        </Link>
+        <Link to="/dead-end">
+          <NoButton> No </NoButton>
+        </Link>
+      </ButtonGroup>
     </div>
-  )
+  );
 }
 
 // return(
